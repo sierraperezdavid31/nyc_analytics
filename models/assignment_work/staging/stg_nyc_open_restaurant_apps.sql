68635764fd1d32bac0e7d771f2638d2d8139a4a4
David Sierra Perez <sierraperezdavid31@gmail.com>
2026-03-28 18:39:23 +0000

    Created a stage NYC Open Restaurant Application SQL file

---
+++ models/assignment_work/staging/stg_nyc_open_restaurant_apps.sql
@@ -0,0 +1,75 @@
+-- Clean and standardize NYC Open Restaurant Applications
+-- One row per service request
+
+WITH source AS (
+   SELECT * FROM {{ source('raw', 'source_nyc_open_restaurant_apps') }}
+), -- Easier to refer to the dbt reference to a long name table this way
+
+cleaned AS (
+   SELECT
+       -- Get all columns from source, except ones we're transforming below
+       -- To do cleaning on them or explicitly cast them as types just in case
+       * EXCEPT (
+           objectid, --
+           globalid, -- 
+           time_of_submission, --
+           zip, --
+           borough, --
+           latitude, --
+           longitude --
+       ),
+
+       -- Identifiers
+       CAST(objectid AS STRING) AS objectid,
+       CAST(globalid AS STRING) AS globalid,
+
+       -- Date/Time
+       CAST(time_of_submission AS TIMESTAMP) AS submission_date,
+
+       -- Location - clean zip code, handling several common zip code data problems
+       CASE
+           WHEN UPPER(TRIM(CAST(zip AS STRING))) IN ('N/A', 'NA') THEN NULL
+           WHEN UPPER(TRIM(CAST(zip AS STRING))) = 'ANONYMOUS' THEN 'Anonymous'
+           WHEN LENGTH(CAST(zip AS STRING)) = 5 THEN CAST(zip AS STRING)
+           WHEN LENGTH(CAST(zip AS STRING)) = 9 THEN CAST(zip AS STRING)
+           WHEN LENGTH(CAST(zip AS STRING)) = 10
+               AND REGEXP_CONTAINS(CAST(zip AS STRING), r'^\d{5}-\d{4}')
+           THEN CAST(zip AS STRING)
+           ELSE NULL
+       END AS zip,
+
+       -- Location - standardized borough, just in case
+       CASE
+           WHEN UPPER(TRIM(borough)) IN ('MANHATTAN', 'NEW YORK COUNTY') THEN 'Manhattan'
+           WHEN UPPER(TRIM(borough)) IN ('BRONX', 'THE BRONX') THEN 'Bronx'
+           WHEN UPPER(TRIM(borough)) IN ('BROOKLYN', 'KINGS COUNTY') THEN 'Brooklyn'
+           WHEN UPPER(TRIM(borough)) IN ('QUEENS', 'QUEEN', 'QUEENS COUNTY') THEN 'Queens'
+           WHEN UPPER(TRIM(borough)) IN ('STATEN ISLAND', 'RICHMOND COUNTY') THEN 'Staten Island'
+           ELSE 'UNKNOWN or CITYWIDE'
+       END AS borough,
+
+
+       CAST(latitude AS DECIMAL) AS latitude,
+       CAST(longitude AS DECIMAL) AS longitude,
+
+       -- Clearer column name as well for this one
+
+       -- Metadata
+       CURRENT_TIMESTAMP() AS _stg_loaded_at
+
+   FROM source
+
+   -- Filters
+   WHERE objectid IS NOT NULL 
+   AND globalid IS NOT NULL
+   AND time_of_submission IS NOT NULL
+   AND CAST(time_of_submission AS TIMESTAMP) >= CAST(DATE_SUB(CURRENT_DATE(), INTERVAL 7 YEAR) AS TIMESTAMP)
+   AND borough IS NOT NULL
+
+   -- Deduplicate
+   QUALIFY ROW_NUMBER() OVER (PARTITION BY objectid ORDER BY created_date DESC) = 1
+)
+
+SELECT * FROM cleaned
+-- All should be part of this table: stg_nyc_311_dot
+
