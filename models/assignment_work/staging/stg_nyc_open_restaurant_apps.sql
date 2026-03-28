0507892461e172626cc51ea9db0b82a1fb380b3f
David Sierra Perez <sierraperezdavid31@gmail.com>
2026-03-28 18:46:16 +0000

    Made more edits to our stg_nyc_open_restaurant_app.sql

--- models/assignment_work/staging/stg_nyc_open_restaurant_apps.sql
+++ models/assignment_work/staging/stg_nyc_open_restaurant_apps.sql
@@ -67,7 +67,7 @@ cleaned AS (
    AND borough IS NOT NULL
 
    -- Deduplicate
-   QUALIFY ROW_NUMBER() OVER (PARTITION BY objectid ORDER BY created_date DESC) = 1
+   QUALIFY ROW_NUMBER() OVER (PARTITION BY objectid ORDER BY time_of_submission DESC) = 1
 )
 
 SELECT * FROM cleaned
