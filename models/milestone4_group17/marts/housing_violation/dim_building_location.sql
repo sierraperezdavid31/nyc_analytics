553c7dbbe0aacc7171f2f7a063f97e1c5478621b
David Sierra Perez <sierraperezdavid31@gmail.com>
2026-04-23 04:44:43 +0000

    edit dim_building_location

--- models/milestone4_group17/marts/housing_violation/dim_building_location.sql
+++ models/milestone4_group17/marts/housing_violation/dim_building_location.sql
@@ -26,24 +26,6 @@ GROUP BY
     --street_code
     --apartment,
     --story
-),
-
-dim_building_location AS (
-    SELECT
-        {{ dbt_utils.generate_surrogate_key([
-            'food_service_establishment',
-            'business_address'
-        ]) }} AS restaurant_key,
-
-        restaurant_name,
-        legal_business_name,
-        doing_business_as_dba,
-        business_address,
-        latitude,
-        longitude,
-        food_service_establishment
-
-    FROM restaurants
 )
 
-SELECT * FROM restaurant_dimension
+SELECT * FROM building_location
