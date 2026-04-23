ac8430e8fb933d6f59abaca62235cf4ab363e24f
David Sierra Perez <sierraperezdavid31@gmail.com>
2026-04-23 04:04:59 +0000

    Made edits to the dim_building_location

---
+++ models/milestone4_group17/marts/housing_violation/dim_building_location.sql
@@ -0,0 +1,17 @@
+-- housing violation dimension: One row per building location
+SELECT
+    -- bin, this field seems to be empty
+    building_id,
+    house_number,
+    street_name,
+    zip, 
+    borough
+
+FROM {{ref('stg_nyc_open_housing_violations')}}
+GROUP BY 
+    building_id,
+    house_number,
+    street_name,
+    zip, 
+    borough
+
