24b1f597b5957732798dde3fdd14e75101be1669
David Sierra Perez <sierraperezdavid31@gmail.com>
2026-04-23 04:43:52 +0000

    edits to building location

--- models/milestone4_group17/marts/housing_violation/dim_building_location.sql
+++ models/milestone4_group17/marts/housing_violation/dim_building_location.sql
@@ -1,14 +1,16 @@
 -- housing violation dimension: One row per building location
+
+WITH building_location AS (
 SELECT
     -- bin, this field seems to be empty
     building_id,
     house_number,
-    low_house_number,
-    high_house_number,
     street_name,
     zip, 
     borough,
-    street_code
+    MAX(low_house_number) as low_house_number,
+    MIN(high_house_number) as high_house_number,
+    MAX(street_code)
     --apartment,
     --story
 
@@ -16,12 +18,32 @@ FROM {{ref('stg_nyc_open_housing_violations')}}
 GROUP BY 
     building_id,
     house_number,
-    low_house_number,
-    high_house_number,
     street_name,
     zip, 
     borough,
-    street_code
+    --low_house_number,
+    --high_house_number,
+    --street_code
     --apartment,
     --story
-ORDER BY building_id ASC
+),
+
+dim_building_location AS (
+    SELECT
+        {{ dbt_utils.generate_surrogate_key([
+            'food_service_establishment',
+            'business_address'
+        ]) }} AS restaurant_key,
+
+        restaurant_name,
+        legal_business_name,
+        doing_business_as_dba,
+        business_address,
+        latitude,
+        longitude,
+        food_service_establishment
+
+    FROM restaurants
+)
+
+SELECT * FROM restaurant_dimension
