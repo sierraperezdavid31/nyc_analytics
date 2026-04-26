8cfdc62ac17d483567870e83ffb0a7b56f68eb0b
David Sierra Perez <sierraperezdavid31@gmail.com>
2026-04-26 02:38:35 +0000

    creating the violation type dimension

---
+++ models/milestone4_group17/marts/housing_violation/dim_violation_type.sql
@@ -0,0 +1,40 @@
+-- violation type dimesion
+
+WITH violation_type_table AS (
+    
+    SELECT
+        -- Adding a new column that was not in the dimension table
+        violation_id,-- column that was not in the original dimension
+        order_number,
+        violation_class,
+        rent_impairing,
+        nov_type,
+        nov_description
+
+    FROM {{ref('stg_nyc_open_housing_violations')}}
+    GROUP BY 
+        violation_id,
+        order_number,
+        violation_class,
+        rent_impairing,
+        nov_type,
+        nov_description
+),
+
+violation_type_dimension AS (
+    SELECT
+        {{
+            dbt_utils.generate_surrogate_key([
+            'violation_id',
+            'order_number',
+            'violation_class',
+            'rent_impairing',
+            'nov_type',
+            'nov_description'
+            ])
+        }}
+
+    FROM violation_type_table
+)
+
+SELECT * FROM violation_type_dimension
