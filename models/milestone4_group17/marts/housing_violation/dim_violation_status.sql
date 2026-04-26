720c111d1f62a27bbdc97bc96f3370176b34a7b4
David Sierra Perez <sierraperezdavid31@gmail.com>
2026-04-26 02:16:25 +0000

    More edits to dim_violation_status

--- models/milestone4_group17/marts/housing_violation/dim_violation_status.sql
+++ models/milestone4_group17/marts/housing_violation/dim_violation_status.sql
@@ -24,11 +24,12 @@ violation_status_dimension AS (
             'violation_status'  
             ])
         }} AS violation_status_key,
+
             current_status_id,
             current_status,
             violation_status
 
     FROM violation_status
-),
+)
 
 SELECT * FROM violation_status_dimension
