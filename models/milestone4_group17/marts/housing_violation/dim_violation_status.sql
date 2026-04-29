ea665c000cef792b14c99e6ba498ff49a669cd9c
David Sierra Perez <sierraperezdavid31@gmail.com>
2026-04-29 02:40:35 +0000

    finalize build for dim_violation_status

--- models/milestone4_group17/marts/housing_violation/dim_violation_status.sql
+++ models/milestone4_group17/marts/housing_violation/dim_violation_status.sql
@@ -5,9 +5,7 @@ WITH violation_status_table AS (
         current_status_id,
         current_status,
         violation_status
-        -- is_close_flag
-        -- violation_status has only two unique values
-        -- closed and open. adding is_close_flag seems redundant
+
     FROM {{ref('stg_nyc_open_housing_violations')}}
     GROUP BY
         current_status_id,
